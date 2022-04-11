6afd909d679781476b99ff21fc3b1dbe6efc80e2
gsfan30 <jeffrohutchens@gmail.com0>
2022-04-10 20:03:32 -0700

    realchain

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,5 +1,5 @@
-const contractAddress = "0x19Df3949e4646283976d14f3Cc02f52A74a518BB"; //Replace with your own contract address
-const chain = 'rinkeby'; // rinkeby or polygon
+const contractAddress = "0x1fA6C5e6C76Bf8c0544f18911e214DF983d12D4c"; //Replace with your own contract address
+const chain = 'polygon'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the Got A Big Head NFT Project!!";
 const welcome_h2 = "Connect to MetaMask to Get Started";
